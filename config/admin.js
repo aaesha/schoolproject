f0fc06dcbd0cde4e804527858108830c14131d02
aaesha <aaeshaalsuhli99@gmail.com>
2024-09-23 21:11:36 +0300

    Configured environment variables and secrets

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET' , 'EzZFzzfy2bOoJ1etFwyVmw=='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT','DZLJ2EIB8BGt4IatDqpg+A=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT' ),
     },
   },
   flags: {
