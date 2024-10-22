fd44de110691924ab2df41825c0a3cacf08614c5
aaesha <62481869+aaesha@users.noreply.github.com>
2024-10-22 10:28:12 +0400

    Update  server.js

--- config/server.js
+++ config/server.js
@@ -1,11 +1,15 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: process.env.PORT || 10000,
+  port: process.env.PORT || 1337,
   app: {
     keys: env.array('APP_KEYS' , ['myKeyA', 'myKeyB']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  afterStart: () => {
+    console.log(`Server running on port ${process.env.PORT || 1337}`);
+  }
 });
+
 
