9fac1e10d9f4680fad2abb4bef05a3afca68781b
aaesha <62481869+aaesha@users.noreply.github.com>
2024-10-22 10:52:23 +0400

    Update variable database.js

--- config/database.js
+++ config/database.js
@@ -22,22 +22,40 @@
 //     },
 // });
 
+// module.exports = ({ env }) => ({
+//     defaultConnection: 'default',
+//     connections: {
+//         default: {
+//             connector: 'bookshelf',
+//             settings: {
+//                 client: 'postgres',
+//                 host: env('DATABASE_HOST', 'aws-0-ap-south-1.pooler.supabase.com'),
+//                 port: env.int('DATABASE_PORT', 5432),
+//                 database: env('DATABASE_NAME', 'postgres'),
+//                 username: env('DATABASE_USERNAME', 'postgres.mifojjzyauienvblcvwc'),
+//                 password: env('DATABASE_PASSWORD', 'ainmnin@school2015'),
+//             },
+//             options: {
+//                 ssl: env.bool('DATABASE_SSL', true),
+//             },
+//         },
+//     },
+// });
 module.exports = ({ env }) => ({
-    defaultConnection: 'default',
-    connections: {
-        default: {
-            connector: 'bookshelf',
-            settings: {
-                client: 'postgres',
-                host: env('DATABASE_HOST', 'aws-0-ap-south-1.pooler.supabase.com'),
-                port: env.int('DATABASE_PORT', 5432),
-                database: env('DATABASE_NAME', 'postgres'),
-                username: env('DATABASE_USERNAME', 'postgres.mifojjzyauienvblcvwc'),
-                password: env('DATABASE_PASSWORD', 'ainmnin@school2015'),
-            },
-            options: {
-                ssl: env.bool('DATABASE_SSL', true),
-            },
-        },
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: 'aws-0-ap-south-1.pooler.supabase.com',
+        port: 5432,
+        database: 'postgres',
+        username: 'postgres.mifojjzyauienvblcvwc',
+        password: 'ainmnin@school2015',
+        ssl: true
+      },
+      options: {},
     },
+  },
 });
